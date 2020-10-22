e9327b29bde558b2bb022c822101338155f84ccf
Alex Wedsday <alexwedsday@hotmail.com>
2020-10-22 17:53:25 -0300

    Adding attribute to QuoteService to learning Tree-Shakeable optimization

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
 import { SharedModule } from '@shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
