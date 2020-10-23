a6f2be42343d0ac1f933082761b54d7ec9b42f99
Bruno Antunes Miranda <bruno.miranda@localiza.com>
2020-10-23 10:15:43 -0300

    LazyLoading nos modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadingStrategy, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -14,7 +14,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
   providers: [],
 })
