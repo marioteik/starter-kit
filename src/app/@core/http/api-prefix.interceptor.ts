a51e71fadedb925bfc69e33d10c04fcec0e12afb
Saulo de Sousa Joseph <saulos.joseph@gmail.com>
2020-10-25 18:02:44 -0300

    refactor: api control

--- src/app/@core/http/api-prefix.interceptor.ts
+++ src/app/@core/http/api-prefix.interceptor.ts
@@ -13,7 +13,11 @@ import { environment } from '@env/environment';
 export class ApiPrefixInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (!/^(http|https):/i.test(request.url)) {
-      request = request.clone({ url: environment.chuckNorrisServerUrl + request.url });
+      if (request.url.includes(`jokes`)) {
+        request = request.clone({ url: environment.chuckNorrisServerUrl + request.url });
+      } else {
+        request = request.clone({ url: environment.gitHubApi + request.url });
+      }
     }
     return next.handle(request);
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
   hmr: false,
   version: env.npm_package_version,
   chuckNorrisServerUrl: 'https://api.chucknorris.io',
+  gitHubApi: 'https://api.github.com',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR'],
 };
