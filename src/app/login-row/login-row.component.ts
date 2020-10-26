e0a95edffe1eb1b8da4943260be43eee2eb2be31
Saulo de Sousa Joseph <saulos.joseph@gmail.com>
2020-10-26 19:33:27 -0300

    feat: ChangeDetection

--- src/app/login-row/login-row.component.ts
+++ src/app/login-row/login-row.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-row',
   templateUrl: './login-row.component.html',
   styleUrls: ['./login-row.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginRowComponent implements OnInit {
   @Input() userLogin: string;
--- src/app/type-row/type-row.component.ts
+++ src/app/type-row/type-row.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-type-row',
   templateUrl: './type-row.component.html',
   styleUrls: ['./type-row.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TypeRowComponent implements OnInit {
   @Input() userType: string;
