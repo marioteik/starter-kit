e1b1c73b36de9fe1e2196e8768fc9b866917715a
Saulo de Sousa Joseph <saulos.joseph@gmail.com>
2020-10-25 21:15:51 -0300

    refactor: use avatar-row in list-github

--- src/app/list-github/list-github.module.ts
+++ src/app/list-github/list-github.module.ts
@@ -4,8 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ListGithubRoutingModule } from './list-github-routing.module';
 import { ListGithubComponent } from './list-github.component';
 
+import { AvatarRowComponent } from '../avatar-row/avatar-row.component';
+
 @NgModule({
-  declarations: [ListGithubComponent],
+  declarations: [ListGithubComponent, AvatarRowComponent],
   imports: [CommonModule, ListGithubRoutingModule],
 })
 export class ListGithubModule {}
