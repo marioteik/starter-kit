ac7b63c86de36b704543334baac2f446aa281f53
Renato Brito <rb.renatobrito@gmail.com>
2020-10-26 19:40:38 -0300

    Inclusao do idioma portugues do Brasil.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
   version: env.npm_package_version + '-dev',
   chuckNorrisServerUrl: '/api',
   defaultLanguage: 'en-US',
-  supportedLanguages: ['en-US', 'fr-FR'],
+  supportedLanguages: ['en-US', 'fr-FR', 'pt-BR'],
 };
